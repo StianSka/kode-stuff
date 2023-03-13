bb4b329f78382a0c6505860ae61b16ec41a47a48
StianSka <116058912+StianSka@users.noreply.github.com>
2023-03-13 10:04:36 +0100

    a

---
+++ modul 3/test c sharp/test c sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace test_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Shalom ingen parametere gitt ");
+            }
+            else
+            {
+                Console.WriteLine("Shalom her kommer paramterene dine ");
+            }
+            for (int i = 0; i<args.Length; i++)
+            {
+                var parameterNo = i + 1;
+                Console.WriteLine("Shalom "+parameterNo+": " + args[i]);
+            }
+            foreach (string text in args)
+            {
+                Console.WriteLine("Shalom "+text);
+            }
+        }
+    }
+}
