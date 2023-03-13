ec0a8c0e14b7e551ccbaf6a88965624ea7fc6dfc
StianSka <116058912+StianSka@users.noreply.github.com>
2023-03-13 14:08:52 +0100

    a

--- modul 3/test c sharp/test c sharp/Converting.cs
+++ modul 3/test c sharp/test c sharp/Converting.cs
@@ -8,5 +8,23 @@ namespace test_c_sharp
 {
     internal class Converting
     {
+        static void Somthing()
+        {
+            // to string
+            // anta variabel x - uansett datatype (av våre valgte datatyper)
+            bool x = true;
+            string s = "" + x;
+            double d = 99.999;
+            string s2 = d.ToString();
+            // to int
+            int i = Convert.ToInt32(d);
+            // to float
+            float f = Convert.ToSingle(i);
+            float f2 = Convert.ToSingle(d);
+            // to double
+            double d2 = Convert.ToDouble(s2);
+
+
+        }
     }
 }
---
+++ modul 3/wiki oppagver/wiki oppagver/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace wiki_oppagver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shalom who are you?");
+            var text = Console.ReadLine();
+            Console.WriteLine("I see you are "+text+" welcom");
+        }
+    }
+}
